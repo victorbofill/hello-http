52f7c8ca3e07c602cb0c8839a30f0fee86d391d4
victorbofill <victorm.bofill@gmail.com>
2018-04-11 09:17:26 -0700

    server.js first draft complete

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server started. Listening on port ${PORT}`);
+});
